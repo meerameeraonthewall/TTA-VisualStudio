4e8894eb95e5fcb7f9350e0bd7788ef7b2587563
meerameeraonthewall <meerag@mit.edu>
2019-03-25 22:24:54 -0700

    Add arithmetic program

---
+++ arithmetic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter an integer.");
+        int numb1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Your number multiplied by 50 is " + (numb1 * 50));
+        Console.WriteLine("Enter an integer.");
+        int numb2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Your number plus 25 is " + (numb2 + 25));
+        Console.WriteLine("Enter an integer.");
+        int numb3 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Your number divided by 12.5 is " + (numb3 / 12.5));
+        Console.WriteLine("Enter an integer.");
+        int numb4 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Is your number greater than 50? " + (numb4>50));
+        Console.WriteLine("Enter an integer.");
+        int numb5 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The remainder after dividing your number by seven is " + (numb5 % 7));
+        Console.Read();
+    }
+}
+
--- dailyreport.cs
+++ dailyreport.cs
@@ -2,23 +2,11 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("The Tech Academy");
-        Console.WriteLine("Student Daily Report");
-        Console.WriteLine("What course are you on?");
-        string course = Console.ReadLine();
-        Console.WriteLine("What page number are you on?");
-        byte page = Convert.ToByte(Console.ReadLine());
-        Console.WriteLine("Do you need help with anything? Please answer true/false");
-        bool help =Convert.ToBoolean( Console.ReadLine());
-        Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
-        string positives = Console.ReadLine();
-        Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
-        string feedback = Console.ReadLine();
-        Console.WriteLine("How many hours did you study today?");
-        byte hours = Convert.ToByte(Console.ReadLine());
-        Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+        Console.WriteLine("Enter an integer.");
+        int numb =  Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Your number multiplied by 50 is " + (numb * 50));
         Console.Read();
 
     }
