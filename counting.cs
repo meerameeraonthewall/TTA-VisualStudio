576e4425ddaed8c04a50ea3d46ebc11fb0db46b4
meerameeraonthewall <meerag@mit.edu>
2019-03-28 11:32:38 -0700

    Revert "Add strings.cs"
    This reverts commit f6f477333a6f8109b301915dbfd502b0e57129ee.

--- counting.cs
+++ counting.cs
@@ -5,13 +5,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter first name.");
-        string fname = Convert.ToString(Console.ReadLine());
-        Console.WriteLine("Enter last name.");
-        string lname = Convert.ToString(Console.ReadLine());
-        Console.WriteLine("Welcome, " + fname + lname);
-        Console.Read();
+        Console.WriteLine("Enter an integer.");
+        int num = Convert.ToInt16(Console.ReadLine());
 
+        while (num >= 0)
+        {
+            Console.WriteLine(num);
+            num --;
+            System.Threading.Thread.Sleep(500);
+        }
+
+        Console.WriteLine("Count to ten!");
+        int i = 0;
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+            System.Threading.Thread.Sleep(500);
+        }
+        while (i < 11);
+        Console.Read();
     }
 }
 
