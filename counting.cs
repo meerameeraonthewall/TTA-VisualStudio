4223a4e0e1a7520b8336b3182e976154aead95b9
meerameeraonthewall <meerag@mit.edu>
2019-03-28 11:02:27 -0700

    Create counting.cs

---
+++ counting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter an integer.");
+        int num = Convert.ToInt16(Console.ReadLine());
+
+        while (num >= 0)
+        {
+            Console.WriteLine(num);
+            num --;
+            System.Threading.Thread.Sleep(500);
+        }
+
+        Console.WriteLine("Count to ten!");
+        int i = 0;
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+            System.Threading.Thread.Sleep(500);
+        }
+        while (i < 11);
+        Console.Read();
+    }
+}
+
