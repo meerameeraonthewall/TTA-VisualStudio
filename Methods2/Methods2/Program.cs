58e755a6097d2d9004d79667ccae4cb8637cb1ca
meerameeraonthewall <42172055+meerameeraonthewall@users.noreply.github.com>
2019-04-01 16:48:34 -0700

    Add Methods2

---
+++ Methods2/Methods2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Function(5, 9);
+        Function(num1: 3, num2: 7);
+        Console.Read();
+    }
+
+    public static void Function(int num1, int num2)
+    {
+        int result = num1 * 7 + 4;
+
+        Console.WriteLine(num2);
+    }
+}
