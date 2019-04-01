ed6f12b0d3ab7c13faaa7e4ba90fdd1ec1f76df4
meerameeraonthewall <42172055+meerameeraonthewall@users.noreply.github.com>
2019-04-01 15:46:52 -0700

    Add Method Exercise Files

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer");
+            int number = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Your number squared is " + Square(number));
+            System.Threading.Thread.Sleep(300);
+            Console.WriteLine("The factorial of your number is " + Factorial(number));
+            System.Threading.Thread.Sleep(300);
+            Console.WriteLine("Your number cubed and added to 8 is " + Formula(number));
+            Console.Read();
+        }
+
+        public static int Square(int number)
+        {
+            int newVal = number * number;
+            return newVal;
+        }
+
+        public static int Factorial(int number)
+        {
+            int newVal = 1;
+
+            while(number >0)
+            {
+                newVal = newVal * number;
+                number--;
+            }
+            return newVal;
+        }
+
+        public static int Formula(int number)
+        {
+            int newVal = number * number * number + 8;
+            return newVal;
+        }
+    }
+}
