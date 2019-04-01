b287ab9cc291f6eae8c1955c3285f55ee078b07e
meerameeraonthewall <meerag@mit.edu>
2019-04-01 14:44:12 -0700

    Create catchblock.cs

---
+++ catchblock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> listy = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Console.WriteLine("Enter an integer by which to divide.");
+        int val = 0;
+
+        try
+        {
+            val = Convert.ToInt16(Console.ReadLine());
+        }
+
+        catch (System.FormatException)
+        {
+            Console.WriteLine("Please enter an integer, not a string.");
+            Console.Read();
+            return;
+        }
+
+            foreach (double item in listy)
+            {
+                Console.WriteLine(item / val);
+                System.Threading.Thread.Sleep(300);
+            }
+
+        Console.WriteLine("{EXIT CATCH BLOCK}");
+        Console.Read();
+    }
+}
