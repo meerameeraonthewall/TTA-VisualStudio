3bb22a390fa0a2e7f638cc4cdb5060d41d2b0546
meerameeraonthewall <42172055+meerameeraonthewall@users.noreply.github.com>
2019-04-02 11:02:35 -0700

    Add Methods3

---
+++ Methods3/Methods3/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods3
+{
+    public class Class1
+    {
+
+        public Class1() {
+
+            Value = true;
+        }
+
+        public bool Value { get; set; }
+
+
+        public static int Function(int num1)
+        {
+            int newVal = num1 * num1 * num1;
+            return newVal;
+        }
+
+        public static int Function(double num2)
+        {
+            int newVal = Convert.ToInt16((num2 * num2 * num2) / 3);
+            return newVal;
+        }
+
+        public static int Function(string num3)
+        {
+            int number = Convert.ToInt16(num3);
+
+            number = number * number * number * number;
+
+            return number;
+        }
+    }
+}
---
+++ Methods3/Methods3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Methods3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+
+            int tryMethod1 = Class1.Function(12);
+            Console.WriteLine(tryMethod1);
+
+            int tryMethod2 = Class1.Function(5.5);
+            Console.WriteLine(tryMethod2);
+
+            int tryMethod3 = Class1.Function("4");
+            Console.WriteLine(tryMethod3);
+
+            Console.Read();
+
+        }
+    }
+}
