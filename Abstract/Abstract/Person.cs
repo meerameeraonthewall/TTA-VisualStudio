44f0d711f5c6046e22f446216e9d5824ce5292a9
meerameeraonthewall <meerag@mit.edu>
2019-04-02 15:57:29 -0700

    Add Abstract

---
+++ Abstract/Abstract/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract
+{
+    public abstract class Person
+    {
+        public string firstName;
+        public string lastName;
+
+
+        public abstract void SayName();
+        
+    }
+}
---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee obj = new Employee();
+            obj.firstName = "Sample";
+            obj.lastName = "Student";
+            obj.SayName();
+            Console.Read();
+        }
+    }
+}
