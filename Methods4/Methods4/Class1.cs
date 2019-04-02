922fd5fe6cb0ae4a83fd01f25876d64a8d38fbea
meerameeraonthewall <meerag@mit.edu>
2019-04-02 12:15:58 -0700

    Add Methods4

---
+++ Methods4/Methods4/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods4
+{
+    class Class1
+    {
+
+        public static int Func1(int num1, int num2 = 1)
+        {
+            int newVal = num1 * num1 * num1 * num2;
+            return newVal;
+        }
+    }
+}
---
+++ Methods4/Methods4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Methods4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 obj = new Class1();
+            int result;
+
+            Console.WriteLine("Please enter an integer.");
+            int val1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Enter a second integer, or press ENTER to use only the first integer.");
+            Object intermediate = Console.ReadLine();
+
+            try
+            {
+                int val2 = Convert.ToInt16(intermediate);
+                result = Class1.Func1(val1, val2);
+            }
+
+            catch (FormatException)
+            {
+                result = Class1.Func1(val1);
+            }
+
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
