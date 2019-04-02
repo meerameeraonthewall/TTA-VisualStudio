24185a9da39e1fb5f649a7ec4520dfd4ae474e8c
meerameeraonthewall <meerag@mit.edu>
2019-04-02 16:46:29 -0700

    Add Interfaces

---
+++ Interfaces/Interfaces/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("I QUIT!!!");
+        }
+    }
+}
---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable obj = new Employee() {firstName = "Sample", lastName = "Student"};
+            obj.Quit();
+
+            Console.Read();
+        }
+    }
+}
