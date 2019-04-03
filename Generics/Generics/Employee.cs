0eff8e0b8cdd6e4da0467d1832f3e58cf141dde7
meerameeraonthewall <meerag@mit.edu>
2019-04-02 19:57:44 -0700

    Add Generics

---
+++ Generics/Generics/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class Employee<T> : Person, IQuittable
+    {
+
+        public int Id;
+        public List<T> things = new List<T>();
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("I QUIT!!!");
+        }
+
+    }
+
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> abc = new Employee<string>() { things= {"apple", "ball", "cowboy", "door", "elephant"} };
+            Employee<int> onetwothree = new Employee<int>() { things = { 1, 2, 3, 4, 5 } };
+
+            foreach (object obj in abc.things)
+            {
+                Console.WriteLine(obj);
+                System.Threading.Thread.Sleep(300);
+            }
+            
+            foreach(object obj in onetwothree.things)
+            {
+                Console.WriteLine(obj);
+                System.Threading.Thread.Sleep(300);
+            }
+
+            Console.Read();
+        }
+    }
+}
