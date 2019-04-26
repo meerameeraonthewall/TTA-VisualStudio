f0a7ab560acea59dda0f53234e29ac9721cf0772
meerameeraonthewall <meerag@mit.edu>
2019-04-26 11:19:16 -0700

    Add Structs project

---
+++ Structs/Structs/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Structs
+{
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Structs
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number val = new Number();
+            val.Amount = 74.25M;
+
+            Console.Write("The Number amount is " + val.Amount);
+            Console.Read();
+
+
+
+
+
+        }
+    }
+}
