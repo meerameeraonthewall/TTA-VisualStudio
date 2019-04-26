a14712294a06cff36f4bce2116058051b6973c8e
meerameeraonthewall <meerag@mit.edu>
2019-04-26 10:56:17 -0700

    Add Enums

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the current day of the week");
+            bool flag = false;
+
+
+            while(flag != true){
+
+                string today = Console.ReadLine();
+
+                try
+                {
+                    Days day = (Days)Enum.Parse(typeof(Days), today);
+                    Console.WriteLine("Today is " + day + "!");
+                    flag = true;
+                    Console.Read();
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an actual day of the week. (try capitalizing the word)");
+        
+                }
+
+            }
+        }
+
+        public enum Days{
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
