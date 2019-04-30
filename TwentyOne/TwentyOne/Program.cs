7e2747e7601c1860c186d6d7610ca8e1d6dbb4cc
meerameeraonthewall <meerag@mit.edu>
2019-04-30 16:02:21 -0700

    Add twentyone game and a bunch of classes

---
+++ TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. What is your name?");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("How much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to play twenty-one?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            
+            if(answer == "yes" || answer == "y" || answer == "yeah")
+            {
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+
+                game -= player;
+                Console.WriteLine("Thank you for playing.");
+             }
+
+            Console.WriteLine("Feel free to look around. Goodbye!");
+            Console.Read();
+        }
+    }
+}
---
+++ TwentyOne/TwentyOne/TwentyOneDealer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwentyOne
+{
+    public class TwentyOneDealer: Dealer
+    {
+        public bool Stay { get; set; }
+        public bool isBusted { get; set; }
+        public List<Card> Hand { get; set; }
+    }
+}
