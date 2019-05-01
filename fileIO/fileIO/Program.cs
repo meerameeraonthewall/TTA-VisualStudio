eccb63b867b44628e9e49fcdf56b1f9777e389ec
meerameeraonthewall <meerag@mit.edu>
2019-04-30 18:05:20 -0700

    Finish 21 game. Add fileIO

---
+++ fileIO/fileIO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace fileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an number.");
+            string num = Console.ReadLine();
+            //File.WriteAllText(@"log.txt", num);
+
+            using (StreamWriter file = new StreamWriter(@"log.txt", true))
+            {
+                file.WriteLine(num);
+                file.WriteLine("\n");
+            }
+
+            string text = File.ReadAllText(@"log.txt");
+            Console.WriteLine(text);
+            Console.Read();
+        }
+    }
+}
