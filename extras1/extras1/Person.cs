be40eeacf42ca80d0e4d56a27565add019c4cbec
meerameeraonthewall <meerag@mit.edu>
2019-05-01 12:46:21 -0700

    Add extras1

---
+++ extras1/extras1/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace extras1
+{
+    public class Person
+    {
+        public Person(string name) : this(name, 0)
+        {
+        }
+
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
---
+++ extras1/extras1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace extras1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string me = "Meera Gregerson";
+            var test = "this is a test";
+
+        }
+    }
+}
